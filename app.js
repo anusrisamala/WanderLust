6ca92f96959ec4ce9c37e08117bc56209fa66fe1
anusrisamala <anusrisamala@gmail.com>
2026-02-25 00:16:07 +0530

    till review validation

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const ejsMate = require('ejs-mate');
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 app.use(express.json());
-const {listingSchema} = require("./schema.js");
-
+const {listingSchema,reviewSchema} = require("./schema.js");
+const Review = require("./models/review.js");
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -51,6 +51,16 @@ app.get("/", (req, res) => {
 
 const validateListing = (req,res,next)=>{
     let {error} =listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }else{
+        next();
+    }
+}
+
+const validateReview = (req,res,next)=>{
+    let {error} =reviewSchema.validate(req.body);
     if(error){
         let errMsg = error.details.map((el)=> el.message).join(",");
         throw new ExpressError(400,errMsg);
@@ -124,6 +134,18 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
 
 }))
 
+//REVIEWS
+app.post("/listings/:id/reviews",validateReview,wrapAsync(async(req,res)=>{
+    let listing =await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+    console.log("saved success");
+    res.redirect(`/listings/${listing._id}`);
+}))
 app.use((req,res,next)=>{
     next(new ExpressError(404,"Page Not Found!"));
 });
